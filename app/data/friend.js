3ef2820a70c36a34a72d022043c5dac10e427c5c
Alan Fountain <alanfountain@Alans-MacBook-Pro.local>
2018-11-30 17:20:28 -0700

    friend app

---
+++ app/data/friend.js
@@ -0,0 +1,21 @@
+var friends = [
+    {
+         name: "Hugh Jackman",
+         photo: "https://www.imdb.com/name/nm0413168/mediaviewer/rm772779264?ref_=nm_ov_ph",
+         scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
+
+    }, {
+         name: "Chris Pratt",
+         photo: "https://en.wikipedia.org/wiki/File:Chris_Pratt_2018.jpg",
+         scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1],
+    }, {
+         name: "Chris Evans",
+         photo: "https://www.imdb.com/name/nm0262635/mediaviewer/rm1966443008?ref_=nm_ov_ph",
+         scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    }, {
+         name: "Scarlett Johansson",
+         photo: "https://www.imdb.com/name/nm0424060/mediaviewer/rm1916122112?ref_=nm_ov_ph",
+         scores: [5, 1, 5, 2, 1, 1, 1, 5, 1, 1],
+    }];
+
+module.exports = friends;
---
+++ app/data/friends.js
@@ -0,0 +1,166 @@
+
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Allen Iverson',
+		photo: 'http://files.shandymedia.com/images/photos/thefumble/allen-iverson-career-high-60-points-dwight-howard.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Alex Morgan',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Alex_Morgan_cropped_England_Women%27s_Vs_USA_%2816367141639%29.jpg/1200px-Alex_Morgan_cropped_England_Women%27s_Vs_USA_%2816367141639%29.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Lebron James',
+		photo: 'http://www.networthq.com/wp-content/uploads/2015/09/LeBron-James-Net-Worth-How-rich-is-How-Much-Money.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Donavan Mitchell',
+		photo: 'https://mk0slamonlinensgt39k.kinstacdn.com/wp-content/uploads/2018/02/GettyImages-917135752.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Cristiano Ronaldo',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/New_Zealand-Portugal_%2814%29.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Peyton Manning',
+		photo: 'http://www.frsportsnews.com/wp-content/uploads/2016/03/miami-united-states-indianapolis-colts.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Carrie Underwood',
+		photo: 'http://media4.s-nbcnews.com/j/newscms/2016_39/1161184/carrie-underwood-2009-05_c681867ab2c5990c9cb0d793d7dd61a1.today-ss-slide-desktop.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Adam Levine',
+		photo: 'https://www.thefamouspeople.com/profiles/images/adam-levine-1.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Freddie Mercury',
+		photo: 'http://i.huffpost.com/gen/1063974/thumbs/o-FREDDIE-MERCURY-PRINCESS-DIANA-facebook.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Elvis Presley',
+		photo: 'https://tse1.mm.bing.net/th?id=OIP.JpK-PgQNzdQl2H36wn8R0QHaIV&pid=Api&w=2250&h=2533&rs=1&p=0',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
